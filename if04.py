ccc2ec54e2434d40b800d76b32a2d287a23cbe66
musayevjahongir <musayevjahongir5051@gmail.com>
2023-09-30 21:29:40 +0500

    musbatni aniqlovchi

--- if04.py
+++ if04.py
@@ -8,4 +8,13 @@ def main(a,b,c):
     returns:
         integer: the number of positive numbers in the given numbers
     """
-    return
+    k=0
+    if a>0:
+        k+=1
+    if b>0:
+        k+=1
+    if c>0:
+        k+=1
+    return k
+print(main(-2,4,1))
+print(main(3,-3,-6))
