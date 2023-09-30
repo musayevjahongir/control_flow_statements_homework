f3c32742f6c6b940142e4b4f5b5d3c6d0093e3ac
musayevjahongir <musayevjahongir5051@gmail.com>
2023-09-30 22:22:22 +0500

    sonni aniqlash

--- if07.py
+++ if07.py
@@ -12,4 +12,16 @@ def main(a):
     Returns:
         string: the message to print
     """
-    return
+    if a>0 and a%2==0:
+        print("musbat juft son ")
+    if a>0 and a%2==1:
+        print("musbat toq son ")
+    if a<0 and a%2==0:
+        print("manfiy juft son ")
+    if a<0 and a%2==1:
+        print("manfiy toq son ")
+    if a==0:
+        print("son 0 ga teng")
+main(57)
+main(-24)
+main(0)
